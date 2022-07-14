628341054ecfd3118d4162111601978d1a8436c9
TimofeyTvorogov <mr.tvorogov2004@mail.ru>
2022-07-14 19:39:20 +0800

    server example

---
+++ demo/src/main/java/com/project/tt/demo/ClientServerApplication.java
@@ -0,0 +1,17 @@
+package com.project.tt.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ClientServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ClientServerApplication.class, args);
+	}
+
+
+}
